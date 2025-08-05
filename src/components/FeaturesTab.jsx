4bb2234a07ab38261f159603e9625ad44ac9af5f
Aditya Phasu <adityaphasu919@gmail.com>
2025-08-06 00:11:19 +0530

    refactor FeaturesTab section layout for desktop

--- src/components/FeaturesTab.jsx
+++ src/components/FeaturesTab.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Illustration1 from "../assets/images/illustration-features-tab-1.svg";
 import Illustration2 from "../assets/images/illustration-features-tab-2.svg";
 import Illustration3 from "../assets/images/illustration-features-tab-3.svg";
+import MoreButton from "./MoreButton";
 
 const features = [
   {
@@ -31,42 +32,53 @@ const FeaturesTab = () => {
   const [activeFeature, setActiveFeature] = useState(features[0]);
 
   return (
-    <section className="overflow-hidden py-23.5 text-center">
-      <div className="px-8 pb-10">
-        <h2 className="text-2xl">Features</h2>
-        <p className="mt-[0.65rem]">
+    <section className="py-23.5 text-center lg:mx-auto lg:max-w-[78.5rem] lg:py-[0.7rem]">
+      <div className="px-8 pb-10 lg:mx-auto lg:max-w-[37rem]">
+        <h2 className="text-2xl lg:text-[2rem]">Features</h2>
+        <p className="mt-[0.65rem] lg:mt-5">
           Our aim is to make it quick and easy for you to access your favourite websites. Your
           bookmarks sync between your devices so you can access them on the go.
         </p>
       </div>
-      <div className="border-light-gray mx-8 border-y">
-        <ul className="divide-light-gray flex flex-col items-center justify-center divide-y-1">
+
+      <div className="border-light-gray lg:border-t-none mx-8 border-t md:border-none lg:mx-auto lg:max-w-[45.7rem]">
+        <ul className="flex flex-col items-center justify-center md:flex-row">
           {features.map((feature, index) => (
-            <li key={index} className="w-full">
+            <li key={index} className="border-light-gray w-full border-b">
               <button
-                className={`relative w-full cursor-pointer py-4 text-[16.75px] transition-all ${
-                  activeFeature.title === feature.title ? "font-medium" : "font-normal"
+                className={`relative w-full cursor-pointer py-4 text-[16.75px] transition-all lg:py-7 ${
+                  activeFeature.title === feature.title
+                    ? "text-light-grayish-blue font-medium"
+                    : "font-normal text-gray-500"
                 }`}
                 onClick={() => setActiveFeature(feature)}
                 aria-selected={activeFeature.title === feature.title}>
                 {feature.title}
                 {activeFeature.title === feature.title && (
-                  <span className="bg-soft-red absolute bottom-[0.5px] left-1/2 h-1 w-36 -translate-x-1/2"></span>
+                  <span className="bg-soft-red absolute bottom-[0.5px] left-1/2 h-1.25 w-36 -translate-x-1/2 lg:w-full"></span>
                 )}
               </button>
             </li>
           ))}
         </ul>
       </div>
-      <div className="relative md:flex">
-        <div className="relative z-20 flex items-center justify-center px-8 py-18">
-          <img src={activeFeature.illustration} alt="Hero" className="z-20 max-w-[19.5rem]" />
-          <div className="bg-soft-blue absolute top-26.75 left-0 z-1 h-[58.9%] w-[82%] rounded-r-full" />
+
+      <div className="relative md:flex md:items-center md:justify-between">
+        <div className="relative z-20 flex items-center justify-center px-8 py-18 lg:ml-11.25 lg:h-[490px]">
+          <img
+            src={activeFeature.illustration}
+            alt="Hero"
+            className={`z-20 max-w-[19.5rem] lg:max-w-full ${activeFeature.title !== "Simple Bookmarking" && "ml-10 lg:ml-18.5"}`}
+          />
+          <div className="bg-soft-blue absolute top-26.75 left-0 z-1 h-[58.9%] w-[82%] rounded-r-full md:top-[9.65rem] lg:-left-34.5 lg:h-88 lg:min-w-160" />
         </div>
 
-        <div className="px-8 py-2">
-          <h3 className="text-2xl">{activeFeature.heading}</h3>
-          <p className="mt-2.5">{activeFeature.description}</p>
+        <div className="px-8 py-2 md:text-left lg:mr-9.75 lg:space-y-[1.36rem] lg:py-10 lg:pt-11">
+          <h3 className="text-2xl lg:text-[2rem]">{activeFeature.heading}</h3>
+          <p className="mt-2.5 lg:max-w-md">{activeFeature.description}</p>
+          <div className="hidden md:block">
+            <MoreButton />
+          </div>
         </div>
       </div>
     </section>
