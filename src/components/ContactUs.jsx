e0117741c107cb97a4c07e760204c5b6d4ea240d
Aditya Phasu <adityaphasu919@gmail.com>
2025-08-07 00:17:04 +0530

    refactor layout and styles for ContactUs and Footer components

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -19,46 +19,50 @@ const ContactUs = () => {
   };
 
   return (
-    <section className="bg-soft-blue flex flex-col items-center justify-center px-6 pt-16.5 pb-12 text-center">
-      <p className="mb-1.5 text-xs tracking-[0.4em] text-white uppercase">35,000+ Already Joined</p>
-      <h2 className="mb-4 text-2xl leading-7.5 font-medium text-white">
-        Stay up-to-date with what we're doing
-      </h2>
-      <form
-        className="flex w-full flex-col items-center px-2 py-3.5"
-        onSubmit={handleSubmit}
-        noValidate>
-        <div className="relative w-full">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              setError(false);
-            }}
-            placeholder="Enter your email address"
-            className={`w-full rounded-sm bg-white p-[0.65rem] placeholder:text-base focus:outline-none ${error ? "border-soft-red border-2 pr-10" : ""}`}
-            aria-invalid={error}
-          />
-          {error && (
-            <span className="absolute top-6 right-4 -translate-y-1/2">
-              <img src={ErrorIcon} />
-            </span>
-          )}
-        </div>
-        {error && (
-          <div className="bg-soft-red -mt-[5px] w-full rounded-b-sm px-2 text-left">
-            <span className="ml-1 text-[11px] text-white italic">
-              Whoops, make sure it's an email
-            </span>
+    <section className="bg-soft-blue flex flex-col items-center justify-center px-6 pt-16.5 pb-12 text-center md:pt-16 md:pb-15">
+      <div className="md:mx-auto md:max-w-114.75">
+        <p className="mb-1.5 text-xs tracking-[0.4em] text-white uppercase md:ml-1.75 md:tracking-[0.48em]">
+          35,000+ Already Joined
+        </p>
+        <h2 className="mb-4 text-2xl leading-7.5 font-medium text-white md:mt-7.75 md:mb-5 md:text-[2rem] md:leading-10">
+          Stay up-to-date with what we're doing
+        </h2>
+        <form
+          className="flex w-full flex-col items-center p-3.5 px-2 md:flex-row md:gap-3.75"
+          onSubmit={handleSubmit}
+          noValidate>
+          <div className="relative w-full">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setError(false);
+              }}
+              placeholder="Enter your email address"
+              className={`w-full rounded-md bg-white p-[0.68rem] placeholder:text-base focus:outline-none md:pl-5 md:placeholder:text-[14.5px] ${error ? "border-soft-red border-2 pr-10" : ""}`}
+              aria-invalid={error}
+            />
+            {error && (
+              <span className="absolute top-6 right-4 -translate-y-1/2">
+                <img src={ErrorIcon} />
+              </span>
+            )}
           </div>
-        )}
-        <button
-          type="submit"
-          className="bg-soft-red mt-4 w-full rounded-sm py-3.5 text-sm font-medium text-white transition-colors hover:bg-red-500">
-          Contact Us
-        </button>
-      </form>
+          {error && (
+            <div className="bg-soft-red -mt-[5px] w-full rounded-b-sm px-2 text-left">
+              <span className="ml-1 text-[11px] text-white italic">
+                Whoops, make sure it's an email
+              </span>
+            </div>
+          )}
+          <button
+            type="submit"
+            className="bg-soft-red mt-4 w-full rounded-md py-3.5 text-sm font-medium text-white transition-colors hover:bg-red-500 md:mt-0 md:basis-[42.5%]">
+            Contact Us
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,27 @@ const links = ["Features", "Pricing", "Contact"];
 
 const Footer = () => {
   return (
-    <footer className="bg-light-grayish-blue flex flex-col items-center gap-[2.18rem] pt-9.5 pb-9">
-      <div>
-        <Logo textFill="#FFF" />
-      </div>
-      <ul className="flex flex-col items-center gap-[1.35rem]">
-        {links.map((link, index) => (
-          <li key={index}>
-            <a
-              href="#"
-              className="text-[15.5px] font-light tracking-widest text-white uppercase transition-colors">
-              {link}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className="py-2">
-        <SocialLinks />
+    <footer className="bg-light-grayish-blue pt-9.5 pb-9 md:py-6">
+      <div className="flex w-full max-w-[76.75rem] flex-col items-center gap-[2.18rem] px-8 md:mx-auto md:flex-row md:justify-between md:gap-16 md:px-15">
+        <div>
+          <Logo textFill="#FFF" />
+        </div>
+        <div className="flex flex-col items-center gap-[2.18rem] md:flex-1 md:flex-row md:justify-between">
+          <ul className="flex flex-col items-center gap-[1.35rem] md:flex-row md:gap-11.25">
+            {links.map((link, index) => (
+              <li key={index}>
+                <a
+                  href="#"
+                  className="text-[15.5px] font-light tracking-widest text-white uppercase transition-colors md:text-[13px] md:font-normal">
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="py-2">
+            <SocialLinks />
+          </div>
+        </div>
       </div>
     </footer>
   );
